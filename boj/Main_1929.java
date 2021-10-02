78746da9408871b8060459254f7b44f0a146dc23
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-10-02 12:12:07 +0900

    1929번 소수구하기

---
+++ boj/Main_1929.java
@@ -0,0 +1,50 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1929 {
+
+    public static boolean[] isNotPrime;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        isNotPrime = new boolean[N + 1];
+
+        checkIsPrime(N);
+
+        printPrime(M,N);
+
+    }
+
+    public static void checkIsPrime(int end) {
+
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        for(int i=2;i<=Math.sqrt(end);i++) {
+            for(int j = i*i; j<end+1; j = j+i) {
+                if(!isNotPrime[j]) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+    }
+
+    public static void printPrime(int start, int end) {
+
+        for(int i = start; i < end + 1; i++) {
+            if(!isNotPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
