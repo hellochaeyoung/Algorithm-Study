102c2c9fb4171a222d720c491f2ebd2e09ddd58d
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-10-13 20:19:50 +0900

    15652번 n 과 m (3)

---
+++ boj/Main_15652.java
@@ -0,0 +1,50 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_15652 {
+
+    static int[] arr;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(1, 0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int start, int depth) {
+
+        if(depth == M) {
+            for(int n : arr) {
+                sb.append(n + " ");
+                //System.out.print(n + " ");
+            }
+            sb.append("\n");
+            //System.out.println();
+            return;
+        }
+
+        for(int i=start;i<=N;i++) {
+
+            arr[depth] = i;
+
+            dfs(start++, depth+1);
+        }
+        return;
+    }
+}
