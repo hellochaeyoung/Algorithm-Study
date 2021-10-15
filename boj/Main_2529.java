affe82eefb6ae2333042b394c09a36f76880a4d5
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-10-15 18:36:24 +0900

    2529번 부등호(2일차_해결)

--- boj/Main_2529.java
+++ boj/Main_2529.java
@@ -4,14 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main_2529 {
 
-    static String[] input;
-    static boolean[] check = new boolean[10];
-    static int[] numbers;
-    static ArrayList<String> list = new ArrayList<>();
+    static String[] input; // 부등호 입력 저장 배열
+    static boolean[] check = new boolean[10]; // 중복 숫자 피하기 위한 check 배열
+    static int[] numbers; // 선택된 숫자 저장할 배열
+    static ArrayList<String> list = new ArrayList<>(); // 최종 수를 저장할 리스트
     static int k;
 
     public static void main(String[] args) throws IOException {
@@ -28,17 +29,17 @@ public class Main_2529 {
         }
 
         for(int i=0;i<10;i++) {
-            dfs(i,0,0);
-            check = new boolean[10];
+            dfs(i,0);
+            check = new boolean[10]; // 다시 false로 초기화
         }
 
-        for(String s : list) {
-            System.out.println(s);
-        }
+        System.out.println(list.get(list.size()-1));
+        System.out.println(list.get(0));
+
 
     }
 
-    static void dfs(int start, int pre, int depth) {
+    static void dfs(int pre, int depth) {
 
         if(depth == k+1) {
             String s = "";
@@ -49,32 +50,37 @@ public class Main_2529 {
             return;
         }
 
+        // 부등호 처리
         String comparator = "";
-        if(start == k) {
-            if(input[start-1].equals(">")){
-                comparator = "<";
-            }else {
-                comparator = ">";
-            }
-        }else {
-            comparator = input[start];
+        if(depth == 0) { // 0일 경우 그대로
+            comparator = input[depth];
+        }else { // 아닐 경우 depth 보다 -1의 부등호를 사용
+            comparator = input[depth-1];
         }
 
 
+        // 부등호는 부등호를 기준으로 왼쪽인지 오른쪽인지 위치에 따라 정반대가 됨
+        // 이 문제에서는 부등호 오른쪽 위치의 숫자를 기준으로 했으므로 > 일때 보다 작은 숫자들을 탐색
+        // < 일때 보다 큰 숫자들을 탐색
         if(comparator.equals(">")) {
-            for(int i=pre+1;i<10;i++) {
+            for(int i=0;i<=pre;i++) { //
                 if(!check[i]) {
                     check[i] = true;
                     numbers[depth] = i;
-                    dfs(start+1,i,depth+1);
+                    dfs(i,depth+1);
+
+                    check[i] = false;
                 }
             }
+
         }else {
-            for(int i=0;i<=pre;i++) {
+            for(int i=pre;i<=9;i++) {
                 if(!check[i]) {
                     check[i] = true;
                     numbers[depth] = i;
-                    dfs(start+1,i,depth+1);
+                    dfs(i,depth+1);
+
+                    check[i] = false;
                 }
             }
         }
