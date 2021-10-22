0b11813665abd65605d0b7b38b1d76c8239d07d3
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-10-22 21:44:56 +0900

    9934번 완전 이진 트리(2일차_해결)

--- boj/Main_9934.java
+++ boj/Main_9934.java
@@ -1,7 +1,6 @@
 package boj;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main_9934 {
@@ -18,9 +17,7 @@ public class Main_9934 {
 
     static int size;
     static int[] arr;
-    //static ArrayList<Node> list = new ArrayList<>();
-    static ArrayList<Integer> list = new ArrayList<>();
-
+    static ArrayList<Node> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,31 +31,25 @@ public class Main_9934 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int middle = count/2; // 최상위 노드의 인덱스
         int depth = 1; // 깊이 즉 레벨
 
-        //list.add(new Node(depth, arr[middle])); // 최상위 노드 추가
-        list.add(arr[middle]);
         size = arr.length-1;
 
-        tree(0, middle-1, depth+1); // 왼쪽 서브 트리
-        tree(middle+1, arr.length-1,depth+1); // 오른쪽 서브 트리
+        tree(0,size, depth);
 
-        for(int i : list) {
-            System.out.println(i);
-        }
-/*
         Collections.sort(list, new Comparator<Node>() { // depth 즉 레벨을 기준으로 정렬
             @Override
             public int compare(Node o1, Node o2) {
                 if(o1.depth < o2.depth) {
                     return -1;
+                }else if(o1.depth == o2.depth) {
+                    return 0;
                 }else {
                     return 1;
                 }
             }
-        });*/
-/*
+        });
+
         int level = list.get(0).depth;
         for(Node n : list) {
             if(n.depth > level) {
@@ -67,39 +58,26 @@ public class Main_9934 {
             }
             System.out.print(n.number + " ");
         }
-*/
+
     }
 
     static void tree(int start, int end,int depth) {
 
-        if(end == 0) { // 최말단 노드일 경우
-            //list.add(new Node(depth, arr[start]));
-            list.add(arr[start]);
-            return;
-        }
-
-        if(start == arr.length-1) { // 최말단 노드일 경우
-            //list.add(new Node(depth, arr[end]));
-            list.add(arr[end]);
-            return;
-        }
-
         int middle = (start + end) / 2;
+        int w = end - start;
 
-        if(middle == 1 || middle == size-1) {
-            //list.add(new Node(depth, arr[middle]));
-            //list.add(new Node(depth+1, arr[start]));
-            //list.add(new Node(depth+1, arr[end]));
-            list.add(arr[middle]);
-            list.add(arr[start]);
-            list.add(arr[end]);
+        if(w == 2) {
+            list.add(new Node(depth, arr[middle]));
+            list.add(new Node(depth+1, arr[start]));
+            list.add(new Node(depth+1, arr[end]));
             return;
         }else {
-            //list.add(new Node(depth, arr[middle]));
-            list.add(arr[middle]);
-            //depth++;
-            tree(0, middle-1, depth+1);
-            tree(middle+1, size, depth+1);
+            list.add(new Node(depth, arr[middle]));
+            
+            tree(start, middle-1, depth+1);
+            tree(middle+1, end, depth+1);
         }
+
     }
+
 }
