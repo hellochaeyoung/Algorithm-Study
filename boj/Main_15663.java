f05ed52bf34f84c8cfaad527793d3356a301f890
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-11-17 00:42:57 +0900

    15663번 n 과 m (9)_2일차_해결

--- boj/Main_15663.java
+++ boj/Main_15663.java
@@ -3,6 +3,10 @@ package boj;
 import java.util.*;
 import java.io.*;
 
+/*
+* TreeSet을 쓰면 안되는 이유 : 기본적으로 오름차순 정렬을 하기 때문에 순서가 틀어질 수 있다.
+* 이미 오름차순으로 배열을 정렬했기 때문에 입력 순서 그대로 저장하는 LinkedHashSet이 적합하다.
+* */
 public class Main_15663 {
 
     static int[] arr;
@@ -10,6 +14,7 @@ public class Main_15663 {
     static int M;
     static boolean[] visited;
     static int[] answer;
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -31,15 +36,20 @@ public class Main_15663 {
         Arrays.sort(arr);
 
         dfs(0);
+
+        for(String s : set) {
+            System.out.println(s);
+        }
     }
 
     static void dfs(int depth) {
 
         if(depth == M) {
+            String str = "";
             for(int i : answer) {
-                System.out.print(i + " ");
+                str += i + " ";
             }
-            System.out.println();
+            set.add(str);
             return;
         }
 
