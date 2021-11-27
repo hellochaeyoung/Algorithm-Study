2b639b965d398c3f350128faf5a3e6ea2807eec7
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-11-27 16:43:38 +0900

    2579번 계단 오르기_3일차(해결)

--- boj/Main_2579.java
+++ boj/Main_2579.java
@@ -3,62 +3,35 @@ package boj;
 import java.io.*;
 import java.util.*;
 
+// DP문제 : 중복되는 연산을 피하기 위한 기법
 public class Main_2579 {
 
     static int stairs = 0;
-    static boolean[] visited;
+    static int[] dp;
     static int[] scores;
-    static ArrayList<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         stairs = Integer.parseInt(br.readLine());
 
-        visited = new boolean[stairs];
+        dp = new int[301];
+        scores = new int[301];
 
-        scores = new int[stairs];
-        for(int i=0;i<stairs;i++) {
+        for(int i=1;i<=stairs;i++) {
             int sc = Integer.parseInt(br.readLine());
             scores[i] = sc;
         }
 
-        climb(0,-1, "");
+        dp[1] = scores[1];
+        dp[2] = scores[1] + scores[2];
+        dp[3] = Math.max(scores[1]+scores[3], scores[2]+scores[3]);
 
-        Collections.sort(list);
-
-        System.out.println(list.get(list.size()-1));
-
-    }
-
-    static void climb(int score, int start, String strs) {
-
-        if(visited[stairs-1]) {
-            list.add(score);
-            return;
+        for(int i=4;i<=stairs;i++) {
+            dp[i] = Math.max(dp[i-3]+scores[i]+scores[i-1], dp[i-2]+scores[i]);
         }
 
-        for(int i=1;i<=2;i++) {
-            int loc = start+i;
-            strs += i;
-
-            if(loc > stairs-1) {
-                return;
-            }
-
-            if(strs.contains("111")) {
-                return;
-            }
-
-            visited[loc] = true;
-            score += scores[loc];
-            climb(score, loc, strs);
-
-            score -= scores[loc];
-            strs = strs.substring(0,strs.length()-1);
-            visited[loc] = false;
-        }
-
-        return;
+        System.out.println(dp[stairs]);
     }
+
 }
