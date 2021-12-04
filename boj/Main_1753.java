bf6c3f8e34f5cc9c9866913c2c37f9b52e2c9813
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-12-04 15:59:14 +0900

    1753번 최단경로_2일차(미해결)

--- boj/Main_1753.java
+++ boj/Main_1753.java
@@ -5,21 +5,37 @@ import java.io.*;
 
 public class Main_1753 {
 
-    static int[][] node;
+    static int V;
+    static int E;
+    static boolean[] visited;
     static int[][] weight;
     static int min = Integer.MAX_VALUE;
+    static int INF = 10000000;
+    static int d[];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken()); // 노드의 수
+        E = Integer.parseInt(st.nextToken()); // 간선의 수
         int K = Integer.parseInt(br.readLine());
 
-        node = new int[V+1][E+1];
+        visited = new boolean[V+1];
         weight = new int[V+1][E+1];
+        d = new int[V+1];
+
+        //초기화
+        for(int i=1;i<=V;i++) {
+            for(int j=1;j<=E;j++) {
+                if(i==j) {
+                    weight[i][j] = 0;
+                }else {
+                    weight[i][j] = INF;
+                }
+            }
+        }
 
         for(int i=0;i<E;i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,10 +43,14 @@ public class Main_1753 {
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            node[u][v] = 1;
             weight[u][v] = w;
+            weight[v][u] = w;
         }
 
+        dijkstra(K);
+        for(int i=1;i<=d.length;i++) {
+            System.out.println(d[i]);
+        }
         /*
         for(int i=1;i<=V;i++) {
             for(int j=1;j<=E;j++) {
@@ -51,16 +71,34 @@ public class Main_1753 {
 
     }
 
-    static void findRoute(int k) {
+    static void dijkstra(int k) {
 
+        for(int i=1;i<=V;i++) {
+            d[i] = weight[k][i];
+        }
+        visited[k] = true;
+        for(int i=0;i<V-2;i++) {
+            int cur = getSmallIndex(k);
+            visited[cur] = true;
+            for(int j=1;j<=V;j++) {
+                if(!visited[j]){
+                    if(d[cur] + weight[k][j] < d[j]) {
+                        d[j] = d[cur] + weight[k][j];
+                    }
+                }
+            }
+        }
     }
 
-    static void getWeight(int u, int v) {
-
-        if(node[u][v] != 0) {
-            min = Math.min(min, weight[u][v]);
-        }else {
-            
+    static int getSmallIndex(int start) {
+        int min = INF;
+        int index = 0;
+        for(int i=1;i<=V;i++) {
+            if(weight[start][i] < min && !visited[i]) {
+                index = i;
+            }
         }
+
+        return index;
     }
 }
