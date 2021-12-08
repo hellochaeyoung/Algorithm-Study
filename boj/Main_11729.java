918b263b0c86f5b9ddd6bb3293623a513f62a215
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-12-08 22:24:20 +0900

    11729번 하노이 탑 이동순서

--- boj/Main_11729.java
+++ boj/Main_11729.java
@@ -6,26 +6,32 @@ import java.io.InputStreamReader;
 
 public class Main_11729 {
 
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
+        sb.append((int)Math.pow(2,N)-1 + "\n");
+
         hanoi(N, 1,2,3);
+
+        System.out.println(sb);
     }
 
-    static void hanoi(int N, int start, int sub, int end) {
+    static void hanoi(int N, int start, int mid, int end) {
 
         if(N==1) {
-            System.out.println(start + " " + end);
+            sb.append(start + " " + end + "\n");
             return;
         }
 
-        hanoi(N-1, start, end, sub);
+        hanoi(N-1, start, end, mid);
 
-        System.out.println(start + " " + end);
+        sb.append(start + " " + end + "\n");
 
-        hanoi(N-1, sub, start, end);
+        hanoi(N-1, mid, start, end);
     }
 }
