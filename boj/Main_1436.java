a90d52113336e19a45cac10a9223be2948e3252d
hellochaeyoung <cyahn0831@hansung.ac.kr>
2021-12-20 17:08:55 +0900

    1436번 영화감독 숌_또다른풀이

--- boj/Main_1436.java
+++ boj/Main_1436.java
@@ -3,7 +3,6 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main_1436 {
 
@@ -12,25 +11,68 @@ public class Main_1436 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int n = 666;
-        int count = 0;
+        int n = 1;
+        int i = 0;
+        int temp = 0;
+
+        // 이 문제의 핵심, 큰 자릿수 부터 조건 따질것!!
         while(true) {
 
-            String s = Integer.toString(n);
-
-            if(s.contains("666")) {
-                count++;
+            if((i % 10000) / 10 == 666 && i % 10 != 6) {
+                for(int j=0;i<1000;j++) {
+                    if(n == N) {
+                        System.out.println(i * 1000 + j);
+                        return;
+                    }
+                    n++;
+                }
             }
 
-            if(count == N) {
-                break;
+            else if(i % 1000 == 666){
+                for(int j=0;j<1000;j++) {
+                    if(n == N) {
+                        System.out.println(i * 1000 + j);
+                        return;
+                    }
+                    n++;
+                }
             }
 
-            n++;
+            else if(i % 100 == 66) {
+                temp = 600;
+                for(int j=0;j<100;j++) {
+                    if(n == N) {
+                        System.out.println(i * 1000 + temp + j);
+                        return;
+                    }
+                    n++;
+                }
+            }
 
+            else if (i % 10 == 6) {
+                temp = 660;
+                for(int j=0;j<10;j++) {
+                    if(n == N) {
+                        System.out.println(i * 1000 + temp + j);
+                        return;
+                    }
+                    n++;
+                }
+
+            }
+
+            else {
+                temp = 666;
+                if(n == N) {
+                    System.out.println(i * 1000 + temp);
+                    return;
+                }
+                n++;
+            }
+
+            i++;
         }
 
-        System.out.println(n);
 
     }
 }
