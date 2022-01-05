25779dd3334683b8bfb2193150bfe3acda8e0a80
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-01-05 17:33:47 +0900

    17780번 새로운 게임_ (해결)

--- boj/Main_17780.java
+++ boj/Main_17780.java
@@ -19,9 +19,9 @@ public class Main_17780 {
 
     static int N,K;
     static int[][] arr;
-    static ArrayList<Horse>[][] board;
-    static boolean isFull = false;
-    static Queue<Horse> queue = new LinkedList<>();
+    static LinkedList<Horse>[][] board;
+    static ArrayList<Horse> list = new ArrayList<>();
+    static int[] dx = {0,1,0,-1}, dy = {1,0,-1,0};
 
     public static void main(String[] args) throws IOException {
 
@@ -31,12 +31,12 @@ public class Main_17780 {
         K = Integer.parseInt(st.nextToken());
 
         arr = new int[N+1][N+1];
-        board = new ArrayList[N+1][N+1];
+        board = new LinkedList[N+1][N+1];
         for(int i=1;i<=N;i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=1;j<=N;j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                board[i][j] = new ArrayList<>();
+                board[i][j] = new LinkedList<>();
             }
         }
 
@@ -46,9 +46,13 @@ public class Main_17780 {
             int y = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
 
+            ////
+            if(d == 1) d = 0;
+            else if(d == 4) d = 1;
+
             Horse horse = new Horse(i+1, x, y, d, i+1);
             board[x][y].add(horse);
-            queue.add(horse);
+            list.add(horse);
         }
 
         play();
@@ -57,131 +61,66 @@ public class Main_17780 {
 
     static void play() {
 
-        int cnt = 0;
-        while(true) {
-            cnt++; // 1부터 N번까지 말이 다 움직여야 1턴이기 때문에
+        for(int c = 1; c <= 1000 ; c++) {
             for(int i=0;i<K;i++) {
-                Horse h = queue.poll();
-                if(h.number == h.parent) { // 다른 말 위에 있다면 두 값이 다름
-                    check(h);
+                Horse h = list.get(i);
 
-                    if(isFull) {
-                        System.out.println(cnt);
-                        System.exit(0);
-                    }
+                int x = h.x;
+                int y = h.y;
+                int d = h.direction;
 
-                    if(cnt > 1000) {
-                        System.out.println(-1);
-                        System.exit(0);
+                if(board[h.x][h.y].get(0).number == i+1) { // 다른 말 위에 있다면 두 값이 다름
+
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+
+                    if(nx > N || nx <= 0 || ny > N || ny <= 0 || arr[nx][ny] == 2) {
+                        board[x][y].get(0).direction = d = (d+2) % 4;
+                        nx = x + dx[d];
+                        ny = y + dy[d];
+
+                        if(nx > N || nx <= 0 || ny > N || ny <= 0 || arr[nx][ny] == 2) {
+                            continue;
+                        }else {
+                            if (move(x,y,nx,ny, arr[nx][ny])) {
+                                System.out.println(c);
+                                return;
+                            }
+                        }
+                    }else {
+                        if(move(x,y,nx,ny,arr[nx][ny])) {
+                            System.out.println(c);
+                            return;
+                        }
                     }
                 }
-                queue.add(h);
             }
-
         }
+        System.out.println(-1);
     }
 
-    static void check(Horse horse) {
+    static boolean move(int x, int y, int nx, int ny, int color) {
 
-        int x = horse.x;
-        int y = horse.y;
-        int d = horse.direction;
-
-        if(d == 1) { // 오른쪽으로 이동
-
-            move(x,y,0,1);
-
-        }else if(d == 2) { // 왼쪽으로 이동
-
-            move(x,y,0,-1);
-
-        }else if(d == 3) { // 위로 이동
-
-            move(x,y,-1,0);
-
-        }else { // 아래로 이동
-
-            move(x,y,1,0);
-
-        }
-
-        return;
-    }
-
-    static void move(int x, int y, int dx, int dy) {
-
-        int nextColor = 0;
-
-        if(x+dx > N || x+dx <=0 || y+dy > N || y+dy <=0) {
-            nextColor = 2;
+        if(color == 0) {
+            while(board[x][y].size() > 0) {
+                Horse h = board[x][y].removeFirst();
+                h.x = nx;
+                h.y = ny;
+                board[nx][ny].add(h);
+            }
         }else {
-            nextColor = arr[x+dx][y+dy];
+            while(board[x][y].size() > 0) {
+                Horse h = board[x][y].removeLast();
+                h.x = nx;
+                h.y = ny;
+                board[nx][ny].add(h);
+            }
         }
 
-        while(true) {
-            if(nextColor == 0) { // 흰색
-
-                for(int i=0;i<board[x][y].size();i++) {
-                    Horse h = board[x][y].remove(i);
-
-                    int parent = 0;
-                    if(board[x+dx][y+dy].size() > 0) {
-                        parent = board[x+dx][y+dy].get(0).number;
-                        h.parent = parent;
-                    }
-
-                    board[x+dx][y+dy].add(h);
-                }
-
-                if(board[x+dx][y+dy].size() >= 4) {
-                    isFull = true;
-                }
-                break;
-
-            }else if(nextColor == 1) { // 빨간색
-
-                for(int i=board[x][y].size()-1;i>=0;i--) {
-                    Horse h = board[x][y].remove(i);
-
-                    int parent = 0;
-                    if(board[x+dx][y+dy].size() > 0) {
-                        parent = board[x+dx][y+dy].get(0).number;
-                        h.parent = parent;
-                    }
-
-                    board[x+dx][y+dy].add(h);
-                }
-
-                if(board[x+dx][y+dy].size() >= 4) {
-                    isFull = true;
-                }
-                break;
-
-            }else { // 파란색
-                dx *= -1;
-                dy *= -1;
-
-                if(x+dx > N || x+dx <= 0 || y+dy > N || y+dy <= 0) { // 방향 반대로 바꾸고 이동할려는 칸이 경계 밖이면
-                    dx *= -1;
-                    dy *= -1;
-
-                    for(int i=0;i<board[x][y].size();i++) {
-                        board[x][y].get(i).direction = dx + dy; // 방향 재설정
-                    }
-                    break;
-                }
-
-                if(arr[x+dx][y+dy] == 2) { // 방향 반대로 바꾸고 이동할려는 칸이 파란색이면
-
-                    for(int i=0;i<board[x][y].size();i++) {
-                        board[x][y].get(i).direction = dx + dy; // 방향 재설정
-                    }
-                    break;
-                }
-
-                nextColor = arr[x+dx][y+dy];
-
-            }
+        if(board[nx][ny].size() >= 4) {
+            return true;
+        }else {
+            return false;
         }
     }
 
