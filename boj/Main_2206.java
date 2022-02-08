9c6fc2dcf86ddc1fd38c22e9d8834a6573b01ba5
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-02-08 15:07:41 +0900

    2206번 벽 부수고 이동하기_해결

--- boj/Main_2206.java
+++ boj/Main_2206.java
@@ -6,21 +6,21 @@ import java.util.*;
 public class Main_2206 {
 
     static class Node {
-        int x,y;
+        int x,y,c;
 
-        public Node(int x, int y) {
+        public Node(int x, int y, int c) {
             this.x = x;
             this.y = y;
+            this.c = c;
         }
     }
 
     static int N, M;
-    static int[][] map;
-    static boolean[][] visited;
+    static char[][] map;
+    static int[][] dist;
+    static boolean[][][] visited;
     static int[] dx = {1,0,-1,0};
     static int[] dy = {0,1,0,-1};
-    static int count = 0;
-    static boolean check = false;
 
     public static void main(String[] args) throws IOException {
 
@@ -29,79 +29,33 @@ public class Main_2206 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M];
+        map = new char[N][M];
+        dist = new int[N][M];
+        visited = new boolean[2][N][M];
 
         for(int i=0;i<N;i++) {
             String str = br.readLine();
             for(int j=0;j<M;j++) {
-                map[i][j] = Integer.parseInt(str.split("")[j]);
+                map[i][j] = str.charAt(j);
             }
         }
 
-        //bfs(0,0);
-
-        visited[0][0] = true;
-        dfs(0,0);
-
-        if(count == 0) {
-            System.out.println(-1);
-        }else {
-            System.out.println(count);
+        if(N-1 == 0 && M-1 == 0) {
+            System.out.println(1);
+            System.exit(0);
         }
 
+        bfs(0,0, 0);
 
     }
 
-    static void dfs(int x, int y) {
-
-        for(int i=0;i<4;i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx == N-1 && ny == M-1) {
-                count++;
-                return;
-            }
-
-            if(nx < 0 || nx >= N || ny < 0 || ny >= M) { // 영역 체크
-                continue;
-            }
-
-            if(map[nx][ny] == 1 && !visited[nx][ny]) {
-
-                if(!check) {
-                    visited[nx][ny] = true;
-                    count++;
-                    check = true;
-
-                    dfs(nx, ny);
-
-                    check = false;
-                    count--;
-                }
-            }
-
-            if(!visited[nx][ny] && map[nx][ny] == 0) {
-                visited[nx][ny] = true;
-                count++;
-                dfs(nx, ny);
-            }
-        }
-    }
-
-    static void bfs(int x, int y) {
+    static void bfs(int x, int y, int c) {
         Queue<Node> q = new LinkedList<>();
 
-        visited[x][y] = true;
-        q.offer(new Node(x,y));
+        q.offer(new Node(x,y,c));
 
         while(!q.isEmpty()) {
             Node node = q.poll();
-
-            if(node.x == N-1 && node.y == M-1) {
-                return;
-            }
 
             for(int i=0;i<4;i++) {
                 int nx = node.x + dx[i];
@@ -111,12 +65,27 @@ public class Main_2206 {
                     continue;
                 }
 
-                if(!visited[nx][ny] && map[nx][ny] == 0) {
-                    visited[nx][ny] = true;
-                    q.offer(new Node(nx, ny));
-                    count++;
+                if(map[nx][ny] == '1') {
+                    if(node.c == 0 && !visited[1][nx][ny]) {
+                        visited[node.c][nx][ny] = true;
+                        dist[nx][ny] = dist[node.x][node.y] + 1;
+                        q.offer(new Node(nx, ny, 1));
+                    }
+                }else {
+                    if(!visited[node.c][nx][ny]) {
+                        visited[node.c][nx][ny] = true;
+                        dist[nx][ny] = dist[node.x][node.y] + 1;
+                        q.offer(new Node(nx, ny, node.c));
+                    }
+                }
+
+                if(nx == N-1 && ny == M-1) {
+                    System.out.println(dist[nx][ny] + 1);
+                    System.exit(0);
                 }
             }
         }
+
+        System.out.println(-1);
     }
 }
