1955cc83850435688153f56c5aeeb20cccbf29fd
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-02-23 16:08:49 +0900

    1800번 인터넷 설치_해결(2일차)

--- boj/Main_1800.java
+++ boj/Main_1800.java
@@ -5,15 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+// 이분 탐색 및 다익스트라
+// 나중에 다시 풀어볼 것...
 public class Main_1800 {
 
+    static class Node implements Comparable<Node>{
+        int end, weight;
+
+        public Node(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+
+        public int compareTo(Node n) {
+            return this.weight > n.weight ? 1 : -1;
+        }
+    }
+
     static int N, P, K;
-    static int[][] graph;
-    static boolean[] visited;
-    static ArrayList<Integer> list = new ArrayList<>();
-    static int min = Integer.MAX_VALUE;
+    static ArrayList<Node>[] map;
 
     public static void main(String[] args) throws IOException {
 
@@ -24,8 +37,12 @@ public class Main_1800 {
         P = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        graph = new int[N+1][N+1];
-        visited = new boolean[N+1];
+        int left = 0, right = 0;
+        int answer = -1;
+        map = new ArrayList[N+1];
+        for(int i=1;i<=N;i++) {
+            map[i] = new ArrayList<>();
+        }
 
         for(int i=0;i<P;i++) {
             st = new StringTokenizer(br.readLine());
@@ -33,46 +50,47 @@ public class Main_1800 {
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            graph[s][v] = w;
-            graph[v][s] = w;
+            map[s].add(new Node(v, w));
+            map[v].add(new Node(s, w));
+            right = Math.max(right, w);
         }
 
-        visited[1] = true;
-        dfs(1);
+        while(left <= right) {
+            int mid = (left + right) / 2;
 
-        if(min > 0) {
-            System.out.println(min);
-        }else {
-            System.out.println(-1);
+            if(dijkstra(mid)) {
+                right = mid - 1;
+                answer = mid;
+            }else {
+                left = mid + 1;
+            }
         }
+
+        System.out.println(answer);
 
     }
 
-    static void dfs(int start) {
-
-        if(start == N) {
-            ArrayList<Integer> tmpList = (ArrayList<Integer>) list.clone();
-
-            Collections.sort(tmpList, Collections.reverseOrder());
-            for(int i=0;i<K;i++) {
-                tmpList.remove(0);
-            }
-
-            if(tmpList.size() > 0) {
-                min = Math.min(min, tmpList.get(0));
-            }
-            return;
+    static boolean dijkstra(int max) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        int[] dist = new int[N+1];
+        for(int i=1;i<=N;i++) {
+            dist[i] = Integer.MAX_VALUE; // 방문 노드 판단 여부로 쓰임
         }
 
-        for(int i=2;i<=N;i++) {
-            if(graph[start][i] != 0 && !visited[i]) {
-                visited[i] = true;
-                list.add(graph[start][i]);
-                dfs(i);
+        dist[1] = 0;
+        queue.add(new Node(1,0));
 
-                list.remove(list.size()-1);
-                visited[i] = false;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            for(Node next : map[node.end]) {
+                if(dist[next.end] > node.weight + (next.weight > max ? 1 : 0)) {
+                    dist[next.end] = node.weight + (next.weight > max ? 1 : 0);
+                    queue.add(new Node(next.end, dist[next.end]));
+                }
             }
         }
+
+        return dist[N] <= K;
     }
 }
