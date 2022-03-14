9a0e57feab4f4379570e2c32d16ba0b428d53750
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-14 20:14:25 +0900

    완료

--- practice/Test2.java
+++ practice/Test2.java
@@ -8,14 +8,29 @@ import java.util.Queue;
 public class Test2 {
 
     static int[][] map;
-    static int N;
-    static int line;
+    static int N, line;
+    static boolean dir;
 
     static List<Integer> list = new ArrayList<>();
 
     // 동, 남, 서, 북
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
+
+    static int setDirection(int d) { //
+
+        if(dir) {
+            d = (d + 1) % 4;
+        }else {
+            if(d == 0) {
+                d = 3;
+            }else {
+                d = (d - 1) % 4;
+            }
+        }
+
+        return d;
+    }
 
     static void bfs(int x, int y, int d) {
 
@@ -26,20 +41,25 @@ public class Test2 {
         Queue<int []> queue = new LinkedList<>();
 
         int num = 1;
+        int cnt = 0;
         map[x][y] = num;
         queue.add(new int[] {x, y, d});
         while(!queue.isEmpty()) {
             int [] arr = queue.poll();
 
+            if(num == line) { //
+                break;
+            }
+
+            if(num == list.get(cnt)) {
+                d = setDirection(d);
+                cnt++;
+            }
+
             int nx = arr[0] + dx[d];
             int ny = arr[1] + dy[d];
 
             map[nx][ny] = ++num;
-
-            if(num == list.get(0)) {
-                d = (d + 1) % 4;
-                list.remove(0);
-            }
 
             queue.add(new int[] {nx, ny, d});
 
@@ -47,20 +67,17 @@ public class Test2 {
 
     }
     public static int[][] solution(int n, boolean clockwise) {
-        int[][] answer = {};
 
         line = (int) Math.ceil((double) (n*n) / 4);
-
         N = n;
         map = new int[N][N];
+        dir = clockwise;
 
         int sum = 0;
         for(int i=N-1;i>=1;i-=2) {
             sum += i;
             list.add(sum);
         }
-        list.add(sum + 1);
-
 
         int start = 0;
         if(!clockwise) {
@@ -77,7 +94,7 @@ public class Test2 {
 
     public static void main(String[] args) {
 
-        int[][] answer = solution(5, true);
+        int[][] answer = solution(9, true);
 
         for(int i=0;i<answer.length;i++) {
             for(int j=0;j<answer.length;j++) {
