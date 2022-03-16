0ba88a4835ddb171957e0773b59099db9c9f4d57
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-03-16 23:48:36 +0900

    완료

--- practice/Test1.java
+++ practice/Test1.java
@@ -1,68 +1,59 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Coin {
+    int won;
+    double cost;
+
+    public Coin(int won, double cost) {
+        this.won = won;
+        this.cost = cost;
+    }
+}
+
 public class Test1 {
 
-    static int N;
-    static int[] arr;
-    static int cost;
-    static int[] costArr;
     static int[] won = {1,5,10,50,100,500};
-    static int min = Integer.MAX_VALUE;
 
-    static void dfs(int result, int answer, int index, int repeat) {
-
-        if(result < 0) {
-            return;
-        }
-
-        if(repeat == -1 && result > 0) {
-            return;
-        }
-
-        if(result == 0) {
-            min = Math.min(min, answer);
-            return;
-        }
-
-        for(int i=repeat;i>=0;i--) {
-
-            result -= won[index] * i; // 생산 비용
-            answer += costArr[index] * i; // 결과
-
-            int next = -1;
-            if(index != 0) {
-                next = result / won[index - 1];
-            }
-
-            dfs(result, answer, index - 1, next);
-
-            //index++;
-
-            result += won[index] * i;
-            answer -= costArr[index] * i;
-
-
-        }
-    }
     public static int solution(int money, int[] costs) {
+        int answer = 0;
 
-        N = costs.length;
-        arr = new int[N];
+        List<Coin> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        cost = money;
-        costArr = costs.clone();
+        for(int i=0;i<won.length;i++) {
+            list.add(new Coin(won[i], (double)costs[i] / won[i]));
+            map.put(won[i], costs[i]);
+        }
 
-        int repeat = money / costs[N-1];
+        list.sort((o1, o2) -> {
+            if(o1.cost == o2.cost) {
+                return Integer.compare(o2.won, o1.won);
+            }
+            return Double.compare(o1.cost, o2.cost);
+        });
 
-        dfs(money,0, N-1, repeat);
+        for (Coin coin : list) {
+            int m = coin.won;
 
-        return min;
+            int mn = money / m;
+            answer += mn * map.get(m);
+            money %= m;
+        }
+
+        return answer;
+
     }
 
     public static void main(String[] args) {
 
-        int money = 4578;
-        int[] costs = {1, 4, 99, 35, 50, 1000};
+        int money = 500;
+        //int[] costs = {1, 4, 99, 35, 50, 1000};
+        int[] costs = {1000,1000,1000,100, 100, 500};
 
         System.out.println(solution(money, costs));
     }
