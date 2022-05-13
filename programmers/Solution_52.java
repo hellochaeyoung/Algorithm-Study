18e51175044096ba2bd0fb637255a3e9a7d3ea52
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-05-13 22:06:05 +0900

    후보키_미해결

--- programmers/Solution_52.java
+++ programmers/Solution_52.java
@@ -4,38 +4,61 @@ import java.util.*;
 
 public class Solution_52 {
 
-    static boolean[] check;
-    static Set<String> set;
-    static String[] temp;
-
     static int N;
+    static boolean[] check;
     static String[] arr;
-
+    static boolean[] isPrimary;
     static List<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String[][] relation = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
 
         solution(relation);
-
-        System.out.println(list.toString());
     }
 
     public static int solution(String[][] relation) {
         int answer = 0;
 
-
-        set = new HashSet<>();
-        temp = new String[relation[0].length];
-
-        arr = new String[relation[0].length];
-
         N = relation[0].length;
+        isPrimary = new boolean[N];
+        arr = new String[N];
         check = new boolean[N];
 
         for(int i=0;i<N;i++) {
             dfs(i+1, 0, 0);
         }
+
+        System.out.println(list.toString());
+
+        List<String> noDuplicatedList = find(relation);
+        System.out.println(noDuplicatedList.toString());
+/*
+        Iterator<String> iterator = noDuplicatedList.iterator();
+        for(int i=0;i< isPrimary.length;i++) {
+            if(isPrimary[i]) {
+                while(iterator.hasNext()) {
+                    String st = iterator.next();
+                    if(st.length() != 1 && st.contains(String.valueOf(i))) {
+                        iterator.remove();
+                    }
+                }
+            }
+        }
+
+ */
+
+        List<String> finalList = new ArrayList<>(noDuplicatedList);
+        for(int i=0;i<noDuplicatedList.size()-1;i++) {
+            String st = noDuplicatedList.get(i);
+            for(int j=i+1;j<noDuplicatedList.size();j++) {
+                String next = noDuplicatedList.get(j);
+                if(next.startsWith(st)) finalList.remove(next);
+            }
+        }
+
+        System.out.println(finalList.toString());
+
+
 
         return answer;
     }
@@ -57,19 +80,19 @@ public class Solution_52 {
                 check[i] = true;
 
                 arr[count] = String.valueOf(i);
-                dfs(depth, count+1, start++);
+                dfs(depth, count+1, i+1);
 
                 check[i] = false;
             }
         }
     }
 
-    static void find(String[][] relations) {
+    static List<String> find(String[][] relations) {
 
         List<String> onlyList = new ArrayList<>();
 
         for(String s : list) {
-            //boolean duplicated = false;
+            boolean duplicated = false;
             String[] arr = s.split("");
 
             Set<String> set = new HashSet<>();
@@ -82,11 +105,21 @@ public class Solution_52 {
                 }
 
                 if (!set.add(sb.toString())) {
+                    duplicated = true;
                     break;
                 }
+            }
 
+            if(!duplicated) {
+                if(s.length() == 1) {
+                    int idx = Integer.parseInt(s);
+                    isPrimary[idx] = true;
+                }
                 onlyList.add(s);
             }
         }
+
+        return onlyList;
     }
+
 }
